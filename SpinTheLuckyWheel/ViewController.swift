173c50b2851b7e7f3e1f7ea3bfa4bd0599e13747
Grey <greyni0330@hmail.com>
2020-02-19 14:40:47 +0800

    Initial Commit

---
+++ SpinTheLuckyWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpinTheLuckyWheel
+//
+//  Created by TheGrey on 2020/2/19.
+//  Copyright © 2020 thegrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
